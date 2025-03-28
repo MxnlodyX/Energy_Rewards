b4e12a9d97e69c8d172c603da53a8bd333ec15c6
Wattanachai Boonchai <wattanachai.work@hotmail.com>
2025-03-29 00:21:34 +0700

    feat(rewards) Merge UI All Reward from feature/all_rewards

--- clean_energy_rewards/lib/pages/user-side/all_rewards.dart
+++ clean_energy_rewards/lib/pages/user-side/all_rewards.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:clean_energy_rewards/pages/components/sideBar.dart';
 import 'package:clean_energy_rewards/pages/components/navBar.dart';
 import 'package:clean_energy_rewards/pages/components/appBar.dart';
@@ -15,10 +14,10 @@ class AllRewards extends StatefulWidget {
 class _AllRewardsState extends State<AllRewards> {
   int _selectedIndex = 2;
   List<RewardModel> rewards = [];
+
   void _getRewards() {
-    setState(() {
-      rewards = RewardModel.getRewards();
-    });
+    rewards = RewardModel.getRewards();
+    setState(() {});
   }
 
   void _onItemTapped(int index) {
@@ -37,13 +36,147 @@ class _AllRewardsState extends State<AllRewards> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            height: MediaQuery.of(context).size.width * 0.9,
-            color: Colors.red,
-          ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 75,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.white,
+
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    blurRadius: 10,
+                    spreadRadius: 2,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Your Point: ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(225, 112, 72, 51),
+                    ),
+                  ),
+                  Image.asset('assets/Energy_Coin.png', width: 45, height: 45),
+                  Text(
+                    " 2500",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color.fromARGB(225, 112, 72, 51),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 70,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Total Reward 100 ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(225, 112, 72, 51),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 142, 180, 134),
+                      padding: EdgeInsets.all(10),
+                    ),
+                    child: Text(
+                      'Reward History',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: GridView.builder(
+                padding: EdgeInsets.all(15),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 0.8,
+                ),
+                itemCount: rewards.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {},
+                    child: Card(
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(15),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  image: DecorationImage(
+                                    image: AssetImage(rewards[index].iconPath),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              rewards[index].name,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Row(
+                              children: [
+                                Image(
+                                  image: AssetImage('assets/Energy_Coin.png'),
+                                  width: 30,
+                                  height: 30,
+                                ),
+                                Text(
+                                  " " + rewards[index].total_point.toString(),
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
       drawer: menuDrawer(),
