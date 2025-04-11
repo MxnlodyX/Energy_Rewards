b73eb540dd426c76fbb1eb99f22c4af71a82686b
Wattanachai Boonchai <wattanachai.work@hotmail.com>
2025-04-12 03:31:23 +0700

    Feat : Fixing Routing

--- clean_energy_rewards/lib/main.dart
+++ clean_energy_rewards/lib/main.dart
@@ -21,7 +21,7 @@ import 'package:clean_energy_rewards/pages/user-side/detail_reward.dart';
 import 'package:clean_energy_rewards/pages/user-side/edit_user_info.dart';
 
 //admin-side
-impo 'package:clean_energy_rewards/pages/admin-side/admin_homepage.dart';
+import 'package:clean_energy_rewards/pages/admin-side/admin_homepage.dart';
 
 void main() async {
   runApp(CleanEnergy());
