fc958a6efddf9461e6391b6034e69f76f4d9e995
Wattanachai Boonchai <wattanachai.work@hotmail.com>
2025-04-17 05:35:34 +0700

    Component : Add new side bar for admin

--- clean_energy_rewards/lib/pages/components/sideBarAdmin.dart
+++ clean_energy_rewards/lib/pages/components/sideBarAdmin.dart
@@ -1,61 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class menuDrawer extends StatelessWidget {
+  Future<void> _logOut(BuildContext context) async {
+    const url = "http://192.168.56.1:4001/api/signOut";
+    var response = await http.post(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        "/LoginGateWay",
+        (route) => false,
+      );
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Logout failed")));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 142, 180, 134),
+      child: Container(
+        color: Color.fromARGB(255, 82, 62, 55),
+        child: ListView(
+          padding: EdgeInsets.only(top: 150),
+          children: [
+            ListTile(
+              textColor: Colors.white,
+              iconColor: Colors.white,
+              leading: Icon(Icons.dashboard),
+              title: Text('Dashboard'),
+              onTap: () {
+                Navigator.pushNamed(context, "/userhomePage");
+              },
             ),
-            child: Text(
-              'Menu',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+            ListTile(
+              textColor: Colors.white,
+              iconColor: Colors.white,
+              leading: Icon(Icons.add_circle_outline),
+              title: Text('Behavior Management'),
+              onTap: () {
+                Navigator.pushNamed(context, "/allUserBehavior");
+              },
             ),
-          ),
-          ListTile(
-            leading: Icon(Icons.home_filled),
-            title: Text('Homepage'),
-            onTap: () {
-              Navigator.pushNamed(context, '/page1');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.account_circle_outlined),
-            title: Text('My Profile'),
-            onTap: () {
-              Navigator.pushNamed(context, '/page2');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.add_circle_outline),
-            title: Text('User behavior'),
-            onTap: () {
-              Navigator.pushNamed(context, '/page3');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.stars),
-            title: Text('Reward Management'),
-            onTap: () {
-              Navigator.pushNamed(context, '/page4');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.exit_to_app),
-            title: Text('Logout'),
-            onTap: () {
-              Navigator.pushNamed(context, '/LoginPage');
-            },
-          ),
-        ],
+            ListTile(
+              textColor: Colors.white,
+              iconColor: Colors.white,
+              leading: Icon(Icons.star),
+              title: Text('Rewards Management'),
+              onTap: () {
+                Navigator.pushNamed(context, "/allReward");
+              },
+            ),
+
+            ListTile(
+              textColor: Colors.white,
+              iconColor: Colors.white,
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Sign out'),
+              onTap: () async {
+                await _logOut(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
